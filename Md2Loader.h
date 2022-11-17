aa37d639cb21f3fab37638a846a035c049575317
Moritz <55063400+Mori-TM@users.noreply.github.com>
2022-11-17 21:16:47 +0100

    Update Md2Loader.h

--- Md2Loader.h
+++ Md2Loader.h
@@ -100,9 +100,26 @@ namespace Md2
 		vec3* NormalList;
 	} ModelData;
 
+	float FastInverseSqrt(float number)
+	{
+		long i;
+		float x2, y;
+		const float threehalfs = 1.5f;
+
+		x2 = number * 0.5f;
+		y = number;
+		i = *(long*)&y;              // evil floating point bit level hacking
+		i = 0x5f3759df - (i >> 1);   // what the fuck? 
+		y = *(float*)&i;
+		y = y * (threehalfs - (x2 * y * y));   // 1st iteration
+		y = y * (threehalfs - (x2 * y * y));   // 2nd iteration, this can be removed - it's for more accuracy
+
+		return y;
+	}
+	
 	void Normalize(vec3* v)
 	{
-		register float Length = glFastInverseSqrt(v->P[0] * v->P[0] + v->P[1] * v->P[1] + v->P[2] * v->P[2]);
+		register float Length = FastInverseSqrt(v->P[0] * v->P[0] + v->P[1] * v->P[1] + v->P[2] * v->P[2]);
 		v->P[0] *= Length;
 		v->P[1] *= Length;
 		v->P[2] *= Length;
